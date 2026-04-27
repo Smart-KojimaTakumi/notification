01af5459bca2d9736239083c0748fd0effa9661e
Smart-KojimaTakumi <takumi.kojima@smart-group.co.jp>
2026-04-27 13:49:28 +0900

    音声出力処理にコメント追加

--- sound.js
+++ sound.js
@@ -1,11 +1,16 @@
+//音声出力処理 メイン関数
 function main(){
+  // ボタンの要素を取得
   const sound_btn = document.getElementById("sound-btn");
-
+  // ボタンクリック後sound関数へ
   sound_btn.addEventListener("click",sound);
 }
 
+//音声出力処理 音声出力関数
 function sound(){
+  // 音声出力したい内容をuttrに代入
   const uttr = new SpeechSynthesisUtterance("集中学習を終了して、アウトプットフォームを記入してください");
+  // uttrをWebSpeachAPIを用いて発声させる。
   speechSynthesis.speak(uttr);
 }
 
